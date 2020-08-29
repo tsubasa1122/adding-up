a7e7732b44c073b55e558495951b4aadc280c9b2
tsubasa1122 <nakanot@open8.com>
2020-08-30 00:10:39 +0900

    ランキング機能実装

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const prefectureDataMap = new Map();
 rl.on('line', (lineString) => {
   const columns = lineString.split(',');
   const year = parseInt(columns[0]);
-  const prefecture = columns[1];
+  const prefecture = columns[2];
   const popu = parseInt(columns[4]);
   if (year === 2010 || year === 2015) {
     let value = prefectureDataMap.get(prefecture);
@@ -33,5 +33,11 @@ rl.on('close', () => {
   for (let [key, value] of prefectureDataMap) {
     value.change = value.popu15 / value.popu10;
   }
-  console.log(prefectureDataMap);
+  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+    return pair2[1].change - pair1[1].change;
+  });
+  const rankingStrings = rankingArray.map(([key, value]) => {
+    return key + ':' + value.popu10 + '=>' + value.popu15 + '変化率:' + value.change;
+  });
+  console.log(rankingStrings);
 });
