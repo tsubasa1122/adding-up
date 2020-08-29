fe6cb7ebdbe269e103c06ea2956d06845dcb2780
tsubasa1122 <nakanot@open8.com>
2020-08-29 23:54:40 +0900

    2010年と2015年のデータを選別出来るように

--- app.js
+++ app.js
@@ -6,5 +6,13 @@ const readline = require('readline');
 const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({'input': rs, 'output': {}});
 rl.on('line', (lineString) => {
-  console.log(lineString);
+  const columns = lineString.split(',');
+  const year = parseInt(columns[0]);
+  const prefecture = columns[1];
+  const popu = parseInt(columns[4]);
+  if(year === 2010 || year === 2015) {
+    console.log(year);
+    console.log(prefecture);
+    console.log(popu);
+  }
 });
