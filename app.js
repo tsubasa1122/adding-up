5e835ea1667c0298e85bfdca3068c84693729dfb
tsubasa1122 <nakanot@open8.com>
2020-08-29 23:58:48 +0900

    変化率が出力されるように

--- app.js
+++ app.js
@@ -5,14 +5,33 @@ const readline = require('readline');
 
 const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({'input': rs, 'output': {}});
+const prefectureDataMap = new Map();
 rl.on('line', (lineString) => {
   const columns = lineString.split(',');
   const year = parseInt(columns[0]);
   const prefecture = columns[1];
   const popu = parseInt(columns[4]);
-  if(year === 2010 || year === 2015) {
-    console.log(year);
-    console.log(prefecture);
-    console.log(popu);
+  if (year === 2010 || year === 2015) {
+    let value = prefectureDataMap.get(prefecture);
+    if (!value) {
+      value = {
+        popu10: 0,
+        popu15: 0,
+        change: null
+      };
+    }
+    if (year === 2010) {
+      value.popu10 = popu;
+    }
+    if (year === 2015) {
+      value.popu15 = popu;
+    }
+    prefectureDataMap.set(prefecture, value);
   }
+});
+rl.on('close', () => {
+  for (let [key, value] of prefectureDataMap) {
+    value.change = value.popu15 / value.popu10;
+  }
+  console.log(prefectureDataMap);
 });
